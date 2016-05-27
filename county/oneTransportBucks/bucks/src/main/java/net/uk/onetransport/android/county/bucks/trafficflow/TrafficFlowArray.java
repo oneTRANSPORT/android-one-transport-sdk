491c13f92a1112e0d1a763a7a449a92b3272feb8
PaulThomas <dr.paul.thomas@gmail.com>
2016-05-27 17:18:51 +0100

    Added further merging of JSON objects.

--- county/oneTransportBucks/bucks/src/main/java/net/uk/onetransport/android/county/bucks/trafficflow/TrafficFlowArray.java
+++ county/oneTransportBucks/bucks/src/main/java/net/uk/onetransport/android/county/bucks/trafficflow/TrafficFlowArray.java
@@ -11,6 +11,8 @@ import net.uk.onetransport.android.county.bucks.BaseArray;
 import net.uk.onetransport.android.county.bucks.R;
 import net.uk.onetransport.android.county.bucks.authentication.CredentialHelper;
 
+import java.util.ArrayList;
+
 public class TrafficFlowArray extends BaseArray implements DougalCallback {
 
     private static final String RETRIEVE_PATH = "BCCTrafficFlowFeedImport/All";
@@ -43,6 +45,7 @@ public class TrafficFlowArray extends BaseArray implements DougalCallback {
         TrafficFlowArray trafficFlowArrayExt = new TrafficFlowArray(
                 GSON.fromJson(contentExt, TrafficFlow[].class));
         merge(trafficFlowArray, trafficFlowArrayExt);
+        internalMerge(trafficFlowArray);
         return trafficFlowArray;
     }
 
@@ -78,6 +81,10 @@ public class TrafficFlowArray extends BaseArray implements DougalCallback {
         return trafficFlows;
     }
 
+    public void setTrafficFlows(TrafficFlow[] trafficFlows) {
+        this.trafficFlows = trafficFlows;
+    }
+
     private static void merge(TrafficFlowArray tfa1, TrafficFlowArray tfa2) {
         for (TrafficFlow tf1 : tfa1.getTrafficFlows()) {
             for (TrafficFlow tf2 : tfa2.getTrafficFlows()) {
@@ -88,8 +95,84 @@ public class TrafficFlowArray extends BaseArray implements DougalCallback {
                     if (tf2.getQueueSeverity() != null) {
                         tf1.setQueueSeverity(tf2.getQueueSeverity());
                     }
+                    if (tf2.getCongestionPercent() != null) {
+                        tf1.setCongestionPercent(tf2.getCongestionPercent());
+                    }
+                    if (tf2.getCurrentFlow() != null) {
+                        tf1.setCurrentFlow(tf2.getCurrentFlow());
+                    }
+                    if (tf2.getAverageSpeed() != null) {
+                        tf1.setAverageSpeed(tf2.getAverageSpeed());
+                    }
+                    if (tf2.getLinkStatus() != null) {
+                        tf1.setLinkStatus(tf2.getLinkStatus());
+                    }
+                    if (tf2.getLinkStatusType() != null) {
+                        tf1.setLinkStatusType(tf2.getLinkStatusType());
+                    }
+                    if (tf2.getLinkTravelTime() != null) {
+                        tf1.setLinkTravelTime(tf2.getLinkTravelTime());
+                    }
                 }
             }
         }
+    }
+
+    private static void internalMerge(TrafficFlowArray tfa) {
+        ArrayList<TrafficFlow> tfList = new ArrayList<>();
+        for (TrafficFlow tf1 : tfa.getTrafficFlows()) {
+            String locationReference = tf1.getLocationReference();
+            int found = -1;
+            for (int i = 0; i < tfList.size() && found < 0; i++) {
+                if (tfList.get(i).getLocationReference().equals(locationReference)) {
+                    found = i;
+                }
+            }
+            if (found == -1) {
+                tfList.add(tf1);
+            } else {
+                TrafficFlow tf2 = tfList.get(found);
+                if (tf1.getQueueSeverity() != null) {
+                    tf2.setQueueSeverity(tf1.getQueueSeverity());
+                }
+                if (tf1.getQueuePresent() != null) {
+                    tf2.setQueuePresent(tf1.getQueuePresent());
+                }
+                if (tf1.getAverageSpeed() != null) {
+                    tf2.setAverageSpeed(tf1.getAverageSpeed());
+                }
+                if (tf1.getAverageVehicleSpeed() != null) {
+                    tf2.setAverageVehicleSpeed(tf1.getAverageVehicleSpeed());
+                }
+                if (tf1.getCongestionPercent() != null) {
+                    tf2.setCongestionPercent(tf1.getCongestionPercent());
+                }
+                if (tf1.getCurrentFlow() != null) {
+                    tf2.setCurrentFlow(tf1.getCurrentFlow());
+                }
+                if (tf1.getFreeFlowSpeed() != null) {
+                    tf2.setFreeFlowSpeed(tf1.getFreeFlowSpeed());
+                }
+                if (tf1.getFreeFlowTravelTime() != null) {
+                    tf2.setFreeFlowTravelTime(tf1.getFreeFlowTravelTime());
+                }
+                if (tf1.getLinkStatus() != null) {
+                    tf2.setLinkStatus(tf1.getLinkStatus());
+                }
+                if (tf1.getLinkStatusType() != null) {
+                    tf2.setLinkStatusType(tf1.getLinkStatusType());
+                }
+                if (tf1.getOccupancy() != null) {
+                    tf2.setOccupancy(tf1.getOccupancy());
+                }
+                if (tf1.getLinkTravelTime() != null) {
+                    tf2.setLinkTravelTime(tf1.getLinkTravelTime());
+                }
+                if (tf1.getVehicleFlow() != null) {
+                    tf2.setVehicleFlow(tf1.getVehicleFlow());
+                }
+            }
+        }
+        tfa.setTrafficFlows(tfList.toArray(new TrafficFlow[tfList.size()]));
     }
 }
