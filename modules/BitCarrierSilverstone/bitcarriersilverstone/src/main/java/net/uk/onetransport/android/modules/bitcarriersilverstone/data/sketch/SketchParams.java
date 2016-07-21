fb0ea09678bb451c41ccb83233a0ee80675634f3
PaulThomas <dr.paul.thomas@gmail.com>
2016-07-21 17:39:56 +0100

    Changed to use correct Silverstone BitCarrier data.

--- modules/BitCarrierSilverstone/bitcarriersilverstone/src/main/java/net/uk/onetransport/android/modules/bitcarriersilverstone/data/sketch/SketchParams.java
+++ modules/BitCarrierSilverstone/bitcarriersilverstone/src/main/java/net/uk/onetransport/android/modules/bitcarriersilverstone/data/sketch/SketchParams.java
@@ -4,8 +4,12 @@ import net.uk.onetransport.android.modules.bitcarriersilverstone.Params;
 
 public interface SketchParams extends Params {
 
-    int[] SKETCH_IDS = {122, 128, 133, 134, 145, 146, 147, 150, 151, 152, 153, 154, 156,
-            157, 161, 165, 175, 176, 183, 193, 195, 196};
+//    int[] SKETCH_IDS = {122, 128, 133, 134, 145, 146, 147, 150, 151, 152, 153, 154, 156,
+//            157, 161, 165, 175, 176, 183, 193, 195, 196};
+//    String RETRIEVE_PREFIX = AE_NAME
+//            + "/BitCarrier/v1.0/InterdigitalDemo/installationtest/data/sketches/s";
+
+    int[] SKETCH_IDS = {2, 4, 5, 7, 8, 10, 15, 18, 20, 21, 23, 28, 31, 32, 38, 39, 42, 44, 45};
     String RETRIEVE_PREFIX = AE_NAME
-            + "/BitCarrier/v1.0/InterdigitalDemo/installationtest/data/sketches/s";
+            + "/BitCarrier/v1.0/InterdigitalDemo/silverstone/data/sketches/s";
 }
