b72fea8f80633c64fae8a41e7b9dfca0f3a49f4e
PaulThomas <dr.paul.thomas@gmail.com>
2016-09-26 16:30:28 +0100

    Moved last updated query to Common module.

--- modules/Common/common/src/main/java/net/uk/onetransport/android/modules/common/provider/CommonContentHelper.java
+++ modules/Common/common/src/main/java/net/uk/onetransport/android/modules/common/provider/CommonContentHelper.java
@@ -1,5 +1,12 @@
 package net.uk.onetransport.android.modules.common.provider;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+
+import net.uk.onetransport.android.modules.common.provider.lastupdated.LastUpdatedContract;
+import net.uk.onetransport.android.modules.common.provider.lastupdated.LastUpdatedProviderModule;
+
 public class CommonContentHelper {
 
     protected static final String CREATION_INTERVAL_SELECTION =
@@ -10,5 +17,12 @@ public class CommonContentHelper {
 
     protected static String[] interval(long oldest, long newest) {
         return new String[]{String.valueOf(oldest), String.valueOf(newest)};
+    }
+
+    public static Cursor getLastUpdated(@NonNull Context context) {
+        return context.getContentResolver().query(LastUpdatedProviderModule.LAST_UPDATED_URI,
+                new String[]{
+                        LastUpdatedContract.LastUpdated.COLUMN_LAST_UPDATE_MILLIS
+                }, null, null, null);
     }
 }
